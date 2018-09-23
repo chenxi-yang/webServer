9a31041b288724c9664eebd2b581ca8920bff1ac
cxyang <yangchenxi1220@gmail.com>
2018-09-23 23:33:11 +0800

    Login2
    Add username and password

--- webServer.py
+++ webServer.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask.ext.login import LoginManager
+from flask import Request
+from flask import Response
 
 app = Flask(__name__)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+
+
 if __name__ == '__main__':
-    app.run(host="10.222.254.12", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)

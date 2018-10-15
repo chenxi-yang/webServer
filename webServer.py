d436606e7efa2caa35891367bcb7efe9eda46388
cxyang <yangchenxi1220@gmail.com>
2018-10-15 22:20:59 +0800

    bug fixed

--- webServer.py
+++ webServer.py
@@ -32,6 +32,29 @@ mysql.init_app(app)
 
 db = mysql.connect()
 
+def get_insert_plan_operation(end_time, plan_name, start_time, user_id):
+    insert_plan_operation = "INSERT INTO user_plan (user_id, start_time, end_time, plan_name) \
+                                    VALUES (%d, '%s', '%s', '%s')" % \
+                            (int(user_id), start_time, end_time, plan_name)
+    return insert_plan_operation
+
+def get_check_res(cursor, username):
+    check_operation = "SELECT * FROM users WHERE user_username = '%s'" % username
+    result = get_one_row(cursor, check_operation)
+    return result
+
+def get_last_insert_id_operation(cursor):
+    last_insert_id_operation = "SELECT MAX(user_id) FROM users;"
+    result_last_insert_id = get_one_row(cursor, last_insert_id_operation)[INSERT_ID]
+    return result_last_insert_id
+
+def get_insert_user_operation(password, username):
+    insert_user_operation = "INSERT INTO users (user_username, user_password) \
+                                            VALUES ('%s', '%s')" % \
+                            (username, password)
+    return insert_user_operation
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -52,53 +75,56 @@ def login():
         username = j_data['username']
         password = j_data['password']
 
+        error_user_id = -1
+        normal_user_id = 0
+
         cursor = db.cursor()
 
         # check whether the user existed before
-        check_operation = "SELECT * FROM users WHERE user_username = '%s'" % username
-        result = get_one_row(cursor, check_operation)
-        user_id = -1
+        result = get_check_res(cursor, username)
+
 
         # sign up
         if result is None:
-            get_last_insert_id_operation = "SELECT MAX(user_id) FROM users;"
-            result_last_insert_id = get_one_row(cursor, get_last_insert_id_operation)[INSERT_ID]
+            result_last_insert_id = get_last_insert_id_operation(cursor)
             print(result_last_insert_id)
-            insert_operation = "INSERT INTO users (user_username, user_password) \
-                                            VALUES ('%s', '%s')" % \
-                               (username, password)
+            insert_user_operation = get_insert_user_operation(password, username)
             try:
-                cursor.execute(insert_operation)
+                cursor.execute(insert_user_operation)
                 db.commit()
             except:
                 db.rollback()
-            user_id = result_last_insert_id + 1
-            ret = {'user_id': result_last_insert_id + 1}
-            return 200, json.dumps(ret)
+            normal_user_id = result_last_insert_id + 1
+            ret = {'user_id': normal_user_id}
+            return json.dumps(ret)
         # log in
         else:
             # valid password
             if password == result[USERS_USER_PASSWORD]:
                 user_id = result[USERS_USER_ID]
                 ret = {'user_id': result[USERS_USER_ID]}
-                return 200, json.dumps(ret)
+                return json.dumps(ret)
             # invalid password: user_id == -1
             else:
-                ret = {'user_id': user_id}
-                return 200, json.dumps(ret)
+                ret = {'user_id': error_user_id}
+                return json.dumps(ret)
 
     return redirect(url_for('/error'))
 
-@app.route('/wrong_password', methods=['POST', 'GET'])
-def wrong_password():
-    # TODO: wrong password handler
-    return 0
 
+# @app.route('/wrong_password', methods=['POST', 'GET'])
+# def wrong_password():
+#     # TODO: wrong password handler
+#     return 'Wrong password'
+
+'''
+Display the plans of a user
+'''
 @app.route('/home/<user_id>')
 def home_user_id(user_id):
     cursor = db.cursor()
 
-    check_operation = "SELECT * FROM users WHERE user_id = '%s'" % user_id
+    check_operation = "SELECT * FROM user_plan WHERE user_id = '%s'" % user_id
     result = get_all_row(cursor, check_operation)
 
     user_plans = []
@@ -110,6 +136,7 @@ def home_user_id(user_id):
     }
 
     for row in result:
+        # print(row)
         plan_dict["plan_id"] = row[USER_PLAN_PLAN_ID]
         plan_dict["plan_name"] = row[USER_PLAN_PLAN_NAME]
         plan_dict["start_time"] = row[USER_PLAN_START_TIME]
@@ -118,12 +145,10 @@ def home_user_id(user_id):
 
     res = json.dumps(user_plans)
 
-    return 200, res
+    return res
 
 @app.route('/home/plan_setting/<user_id>', methods=['POST', 'GET'])
 def home_plan_setting(user_id):
-    # insert into user_plan
-    cursor = db.cursor()
     if request.method == 'POST':
         data = request.data
         j_data = json.loads(data)
@@ -131,23 +156,23 @@ def home_plan_setting(user_id):
         end_time = j_data['end_time']
         plan_name = j_data['plan_name']
 
-        insert_plan_operation = "INSERT INTO user_plan \
-                                    VALUES ('%s', '%s', '%s')" % \
-                                    (start_time, end_time, plan_name)
+        cursor = db.cursor()
+        insert_plan_operation = get_insert_plan_operation(end_time, plan_name, start_time, user_id)
+        # print(insert_plan_operation)
         try:
             cursor.execute(insert_plan_operation)
             db.commit()
         except:
             db.rollback()
-            return 404, 'Plan set error'
+            return 'Plan set error'
 
-    return 200, 'Plan set successfully'
+    return 'Plan set successfully'
 
 # TODO: get users' app usage history automatically
 
 @app.route('/error')
 def error():
-    return 404, 'Error'
+    return 'Error'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
